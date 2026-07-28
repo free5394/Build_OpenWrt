244ff94276c126b1efd3246a4b5454c9ce5edd77
walk6834 <glacier0315@126.com>
2026-07-28 14:43:30 +0800

    chore(build scripts): 简化并重构构建脚本
    1. 移除冗余的严格模式、日志重定向等前置脚本代码
    2. 简化下载和编译的日志输出方式
    3. 简化配置文件生成流程，直接使用full.config

--- build.sh
+++ build.sh
@@ -26,24 +26,23 @@ mkdir -p logs
 echo "更新feeds并安装..."
 ./custom_scripts/apply_custom_feeds.sh
 
-echo "生成配置文件..."
-make menuconfig
-./scripts/diffconfig.sh >default.config
+# echo "生成配置文件..."
+# make menuconfig
+# ./scripts/diffconfig.sh >default.config
 
-# echo "应用配置文件..."
-# cp -f standard.config .config
-# make defconfig
+echo "配置文件..."
+cp -f full.config .config && make defconfig
 
 echo "应用自定义设置..."
 ./custom_scripts/apply_custom_settings.sh
 ./custom_scripts/patch_custom_settings.sh
 
 echo "开始下载依赖..."
-(make download -j $(($(nproc) + 1)) V=s || make download -j1 V=s) 1>logs/download.txt 2>&1
+make download -j $(($(nproc) + 1)) V=s || make download -j1 V=s
 
 echo "开始编译OpenWrt..."
 echo $(date "+%Y-%m-%d %H:%M:%S start") >build.txt
-(make -j $(($(nproc) + 1)) V=s || make -j1 V=s) 1>logs/openwrt.txt 2>&1
+make -j $(($(nproc) + 1)) V=s || make -j1 V=s
 echo $(date "+%Y-%m-%d %H:%M:%S end") >>build.txt
 
 echo "开始上传..."
