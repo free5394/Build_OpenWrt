1d29638e0fc20763fd12f7a3fce88bb80cab0801
walk6834 <glacier0315@126.com>
2026-07-29 12:00:41 +0800

    refactor(scripts): 重构自定义feeds脚本，合并冗余函数
    重构了自定义feeds应用脚本，移除了冗余的update_feeds和install_feeds独立函数，将逻辑内联到主流程中，同时调整了自定义插件克隆相关的冗余代码，简化脚本结构提升可维护性

--- x86-64/custom_scripts/apply_custom_feeds.sh
+++ x86-64/custom_scripts/apply_custom_feeds.sh
@@ -46,18 +46,6 @@ add_feed() {
 	echo "[新增] $feed_line"
 }
 
-# 更新&安装插件
-update_feeds() {
-	echo "更新feeds..."
-	./scripts/feeds update -a
-}
-
-# 更新&安装插件
-install_feeds() {
-	echo "安装feeds..."
-	./scripts/feeds install -a
-}
-
 # 克隆Git仓库到指定目录
 # 参数1: 仓库URL（必选） 参数2: 分支名（可选） 参数3: 目标目录（必选）
 clone_repo() {
@@ -78,42 +66,14 @@ clone_repo() {
 
 }
 
-# 克隆自定义插件
-clone_packages1() {
-	echo "克隆自定义插件..."
-	git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git feeds/packages/kenzo
-	git clone --depth 1 https://github.com/kenzok8/small.git feeds/packages/small
-
-	# 移除 openwrt feeds 自带的核心库
-	rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-	git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/passwall-packages
-
-	# 移除 openwrt feeds 过时的luci版本
-	rm -rf feeds/luci/applications/luci-app-passwall
-	git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git package/passwall-luci
-
-	git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
-	git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git package/momo
-}
-
-# 添加自定义Feeds
-add_custom_feeds1() {
-	add_feed "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" '1i'
-	add_feed "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" '2i'
-	add_feed "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
-	add_feed "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main"
-
-	update_feeds
-	install_feeds
-}
-
 # 添加自定义Feeds
 add_custom_feeds() {
 	# 添加自定义Feeds
 	add_feed "src-git kenzo https://github.com/kenzok8/openwrt-packages" '1i'
 	add_feed "src-git small https://github.com/kenzok8/small" '2i'
 
-	update_feeds
+	echo "更新feeds..."
+	./scripts/feeds update -a
 
 	echo "删除冲突的插件..."
 	rm -rf feeds/luci/applications/luci-app-mosdns
@@ -123,7 +83,8 @@ add_custom_feeds() {
 	rm -rf feeds/packages/lang/golang
 	git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
 
-	install_feeds
+	echo "安装feeds..."
+	./scripts/feeds install -a
 }
 
 # 主函数
