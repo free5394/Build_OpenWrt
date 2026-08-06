b6909963144c90df0bfb7366d6a93851f1be47d7
walk6834 <glacier0315@126.com>
2026-08-06 14:01:49 +0800

    fix(x86-64构建): 移除编译报错的luci-app-eqos插件
    注释掉的删除操作改为实际执行，解决kenzo源中luci-app-eqos和官方插件冲突导致的编译失败问题

--- build/x86-64/custom_scripts/apply_custom_feeds.sh
+++ build/x86-64/custom_scripts/apply_custom_feeds.sh
@@ -229,11 +229,9 @@ update_package() {
 	clone_repo "https://github.com/kenzok8/openwrt-packages" "$community_dir/kenzo"
 	clone_repo "https://github.com/kenzok8/small" "$community_dir/small"
 
-	# log_info "删除出错插件"
-	# luci-app-fchomo: small feed 中此插件编译报错，等待上游修复后可移除此行
-	# rm -rf "$community_dir"/small/luci-app-fchomo
+	log_info "删除出错插件"
 	# luci-app-eqos: kenzo feed 中此插件与官方 luci-app-eqos 冲突导致编译报错，等待上游修复后可移除此行
-	# rm -rf "$community_dir"/kenzo/luci-app-eqos
+	rm -rf "$community_dir"/kenzo/luci-app-eqos
 
 	log_info "覆盖原始package目录"
 	update_matching_dirs "$community_dir/kenzo" "feeds"
